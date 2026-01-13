f354bb48eb240e992191217833e8349dcae18451
yachikajain9 <yachikajain9@gmail.com>
2026-01-14 01:15:44 +0530

    Add files via upload

---
+++ practical8.java
@@ -0,0 +1,18 @@
+
+
+public class Practical8{
+	public static void main(String[] args){
+		
+		System.out.println("\nyachika ");
+		System.out.println("Enrollment No.: 240390107056\n");
+			int a = Integer.parseInt(args[0]);
+			int b = Integer.parseInt(args[1]);
+			
+			int sum = a+b;
+			int mul = a*b;
+			System.out.println("Sum : "+sum);
+	}
+
+} 
+    
+
