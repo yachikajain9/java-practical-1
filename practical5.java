3fec3620e54a98a3780f23c6a8f4342651d1f573
yachikajain9 <yachikajain9@gmail.com>
2026-04-23 21:32:05 +0530

    Update practical5.java

--- practical5.java
+++ practical5.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Practical5{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		System.out.println("\nBhoomika Maheshwari");
 		System.out.println("Enrollment No.: 240390107056\n");
 		System.out.println("Enter Any integer Amount To Be Dispensed: ");
 		int a = sc.nextInt();
